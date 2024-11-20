368062bf2916bad74a1487dc32bbd04dd0913e8b
Allen <ppallen032469@gmail.com>
2024-11-20 18:35:05 +0800

    添加dependency dev tools

--- src/main/java/com/allen/springbootmall/controller/UserController.java
+++ src/main/java/com/allen/springbootmall/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173/")
+//@CrossOrigin(origins = "http://localhost:5173/")
 public class UserController {
 
     @Autowired
